2680a78a937822e2c7ca29ca4cdc5c56d6447b6e
capiu957 <victordesouz5827@gmail.com>
2024-12-10 14:43:42 -0300

    Add files via upload

--- calc1.py
+++ calc1.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as st # type: ignore
 
 def calculate(num1, num2, operation):
     try:
@@ -19,7 +19,7 @@ def calculate(num1, num2, operation):
 
 # Streamlit app
 st.title('Basic Calculator')
-st.write('Choose two numbers and the operation you want to do.')
+#st.write('Choose two numbers and the operation you want to do.')
 
 # User inputs
 num1 = st.text_input('Type in the first number')
