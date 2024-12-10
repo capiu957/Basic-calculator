4baa2791b1a11be29afede4106e16e0647172b0e
capiu957 <victordesouz5827@gmail.com>
2024-12-10 15:25:36 -0300

    Add files via upload

--- calc1.py
+++ calc1.py
@@ -1,35 +1,31 @@
 import streamlit as st
 
 def calculate(num1, num2, operation):
+    
     try:
         num1 = float(num1)
         num2 = float(num2)
+        
         if operation == 'Sum':
             return num1 + num2
         elif operation == 'Sub':
             return num1 - num2
-        elif operation == 'Mult':
+        elif operation == 'Mult': 
             return num1 * num2
         elif operation == 'Div':
-            if num2 == 0:
+             if num2 == 0:
                 return 'Error: division by zero!'
-            return num1 / num2
+             return num1 / num2
     except ValueError:
-        return 'Error: invalid input! Please insert numbers.'
+        return 'Error: invalid input!Please insert numbers'
 
-# Streamlit app
 st.title('Basic Calculator')
-st.write('Choose two numbers and the operation you want to perform.')
+st.write('Choose two numbers and the operation you want to do')
 
-# User inputs
 num1 = st.text_input('Type in the first number')
 num2 = st.text_input('Type in the second number')
 operation = st.selectbox('Choose the operation:', ['Sum', 'Sub', 'Mult', 'Div'])
 
-# Calculation and output
 if st.button('Calculate'):
-    result = calculate(num1, num2, operation)
-    if result is not None:
-        st.write(f"Result: {result}")
-    else:
-        st.write("No result was calculated.")
+   result = calculate(num1, num2, operation)
+   st.write (f"Result: {result}")
